33c517b81b09dcab3bd2865a244758674d18abe4
Mathieu Ducharme <mat@locomotive.ca>
2015-11-13 10:37:12 -0500

    Fix boilerplate front controller. (default error handler, config moved to charcoal/app/config in container.

--- www/index.php
+++ www/index.php
@@ -9,7 +9,7 @@ use \Slim\Container as SlimContainer;
 
 
 // If using PHP's built-in server, return false for existing files on filesystem
-if (php_sapi_name() === 'cli-server') {
+if (PHP_SAPI === 'cli-server') {
     $filename = __DIR__ . preg_replace('#(\?.*)$#', '', $_SERVER['REQUEST_URI']);
     if (is_file($filename)) {
         return false;
@@ -19,28 +19,42 @@ if (php_sapi_name() === 'cli-server') {
 /** Require the Charcoal Framework */
 include '../vendor/autoload.php';
 
-// Create container and configure it (with charcoal-config)
-$container = new SlimContainer();
+$configuration = [
+    'settings' => [
+        'displayErrorDetails' => true,
+    ],
+];
 
-$container['config'] = function($c) {
+// Create container and configure it (with charcoal-config)
+$container = new SlimContainer($configuration);
+
+$container['charcoal/app/config'] = function($c) {
     $config = new AppConfig();
     $config->add_file(__DIR__.'/../config/config.php');
     return $config;
 };
+
+// $container['errorHandler'] = function ($c) {
+//     return function ($request, $response, $exception) use ($c) {
+//     	var_dump($exception);
+//         return $c['response']->withStatus(500)
+// 			->withHeader('Content-Type', 'text/html')
+// 			->write('Something went wrong!');
+// 	};
+// };
 
 // Slim is the main app
 $app = new SlimApp($container);
 
 // Set up dependencies
 require __DIR__.'/../config/dependencies.php';
-// Register middleware
+// Register middlewares
 require __DIR__.'/../config/middlewares.php';
 // Register routes
 require __DIR__.'/../config/routes.php';
 
-$charcoal = new CharcoalApp($container['config'], $app);
+$charcoal = new CharcoalApp($container['charcoal/app/config'], $app);
 $charcoal->setup();
-
 
 $app->run();
 
