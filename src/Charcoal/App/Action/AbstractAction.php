209a8a600b4f5e926f77cc78ded9ce63e389c6d5
Mathieu Ducharme <mat@locomotive.ca>
2015-12-01 00:48:11 -0500

    Add PSR7 signature to actions. Add redirect URLs (failure + success). Magic invoke methods is now final.

--- src/Charcoal/App/Action/AbstractAction.php
+++ src/Charcoal/App/Action/AbstractAction.php
@@ -5,6 +5,10 @@ namespace Charcoal\App\Action;
 // Dependencies from `PHP`
 use \InvalidArgumentException;
 
+// PSR-7 (http messaging) dependencies
+use \Psr\Http\Message\RequestInterface;
+use \Psr\Http\Message\ResponseInterface;
+
 // Local namespace dependencies
 use \Charcoal\App\Action\ActionInterface;
 
@@ -15,9 +19,12 @@ abstract class AbstractAction implements ActionInterface
 {
     const MODE_JSON = 'json';
     const MODE_REDIRECT = 'redirect';
-    const MODE_BOOLEAN = 'boolean';
-    const MODE_OUTPUT = 'output';
-    const DEFAULT_MODE = self::MODE_REDIRECT;
+    const DEFAULT_MODE = self::MODE_JSON;
+
+    /**
+     * @var string $lang
+     */
+    private $lang;
 
     /**
      * @var string $mode
@@ -29,6 +36,43 @@ abstract class AbstractAction implements ActionInterface
      */
     private $success = false;
 
+    /**
+     * @var TranslationString $success_url
+     */
+    private $success_url;
+
+    /**
+     * @var TranslationString $failure_url
+     */
+    private $failure_url;
+
+    /**
+     * @param RequestInterface  $request  A PSR-7 compatible Request instance.
+     * @param ResponseInterface $response A PSR-7 compatible Response instance.
+     * @return ResponseInterface
+     */
+    final public function __invoke(RequestInterface $request, ResponseInterface $response)
+    {
+        $response = $this->run($request, $response);
+
+        switch ($this->mode()) {
+            case self::MODE_JSON:
+                $response = $response
+                    ->withHeader('Content-Type', 'application/json')
+                    ->write(json_encode($this->response()));
+                break;
+
+            case self::MODE_REDIRECT:
+                $response = $response
+                    ->withStatus(301)
+                    ->withHeader('Location', $this->redirect_url());
+                break;
+        }
+
+        return $response;
+    }
+
+    
     /**
      * @param array $data The data to set.
      * @return AbstractAction Chainable
@@ -50,6 +94,30 @@ abstract class AbstractAction implements ActionInterface
             }
         }
         return $this;
+    }
+
+    /**
+     * @param string $lang The langauge code for current action.
+     * @throws InvalidArgumentException If the lang parameter is not a string.
+     * @return ActionInterface Chainable
+     */
+    public function set_lang($lang)
+    {
+        if (!is_string($lang)) {
+            throw new InvalidArgumentException(
+                'Can not set language, lang parameter needs to be a string.'
+            );
+        }
+        $this->lang = $lang;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function lang()
+    {
+        return $this->lang;
     }
 
     /**
@@ -101,7 +169,69 @@ abstract class AbstractAction implements ActionInterface
     }
 
     /**
+     * @param mixed $url The success URL (translation string).
+     * @return ActionInterface Chainable
+     */
+    public function set_success_url($url)
+    {
+        $this->success_url = new TranslationString($url);
+        return $this;
+    }
+
+    /**
+     * @return TranslationString
+     */
+    public function success_url()
+    {
+        return $this->succes_url;
+    }
+
+    /**
+     * @param mixed $url The success URL (translation string).
+     * @return ActionInterface Chainable
+     */
+    public function set_failure_url($url)
+    {
+        $this->failure_url = new TranslationString($url);
+        return $this;
+    }
+
+    /**
+     * @return TranslationString
+     */
+    public function failure_url()
+    {
+        return $this->failre_url;
+    }
+
+    /**
      * @return string
      */
+    public function redirect_url()
+    {
+        if ($this->success() === true) {
+            $url = $this->success_url();
+        } else {
+            $url = $this->failure_url();
+        }
+
+        // Get the translated URL
+        $url = $url->val($this->lang());
+        if (!$url) {
+            $url = '';
+        }
+        return $url;
+    }
+
+    /**
+     * @return array
+     */
     abstract public function response();
+
+    /**
+     * @param RequestInterface  $request  A PSR-7 compatible Request instance.
+     * @param ResponseInterface $response A PSR-7 compatible Response instance.
+     * @return ResponseInterface
+     */
+    abstract public function run(RequestInterface $request, ResponseInterface $response);
 }
