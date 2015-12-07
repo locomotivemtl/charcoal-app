a4d2f9de11e46a7368f2e8d0acd6bd2a7a5d3d43
Chauncey McAskill <chauncey@locomotive.ca>
2015-12-07 16:31:35 -0500

    Revised LanguageManager.
    Changes:
    - Updated to support `MultilingualAwareInterface` and `ConfigurableTranslationTrait`;
    - Redefined setup methods;
      - `Language` making and `TranslationConfig` instantiation now under `setup_languages()`;
      - Instantiation of a global `Catalog` added under `setup_translations()`;
    - Removed methods now provided by `ConfigurableTranslationTrait`.

--- src/Charcoal/App/Language/LanguageManager.php
+++ src/Charcoal/App/Language/LanguageManager.php
@@ -2,36 +2,43 @@
 
 namespace Charcoal\App\Language;
 
-// PHP Dependencies
 use \InvalidArgumentException;
+use \RuntimeException;
 
-// Module `charcoal-config` dependencies
+// Intra-module (`charcoal-app`) dependency
+use \Charcoal\App\AbstractManager;
+
+// Intra-module (`charcoal-config`) dependency
 use \Charcoal\Config\GenericConfig;
 
-// Module `charcoal-core` dependencies
+// Intra-module (`charcoal-core`) dependencies
+use \Charcoal\Translation\Catalog;
+use \Charcoal\Translation\CatalogInterface;
+use \Charcoal\Translation\ConfigurableTranslationTrait;
+use \Charcoal\Translation\MultilingualAwareInterface;
 use \Charcoal\Translation\TranslationConfig;
+use \Charcoal\Translation\TranslationString;
+use \Charcoal\Translation\TranslationStringInterface;
 
-// Intra-module (`charcoal-app`) dependencies
-use \Charcoal\App\AbstractManager;
+// Local namespace dependencies
 use \Charcoal\App\Language\Language;
 use \Charcoal\App\Language\LanguageInterface;
 
 /**
+ * Manage a collection of LanguageInterface objects and a unique TranslationConfig object.
  *
+ * Not implementing ConfigurableInterface because the AbstractManager shouldn't permit
+ * it's properties to be modified from outside the manager.
  */
-class LanguageManager extends AbstractManager
+class LanguageManager extends AbstractManager implements
+    MultilingualAwareInterface
 {
-    /**
-     * @var array LanguageInterface
-     */
-    private $languages;
+    use ConfigurableTranslationTrait;
 
     /**
-     * @var TranslationConfig
-     */
-    private $translation_config;
-
-    /**
+     * A static cache of language information, raw data
+     * used to create fill-up Language objects.
+     *
      * @var GenericConfig
      */
     private static $language_index;
@@ -39,12 +46,18 @@ class LanguageManager extends AbstractManager
     /**
      * Set up the available languages, defaults, and active
      *
-     * @return void
+     * @return self
      */
     public function setup()
     {
-        $this->setup_translations();
-        $this->setup_languages();
+        $config = $this->config();
+
+        if (!($config instanceof TranslationConfig)) {
+            $this->setup_languages($config);
+            $this->setup_translations($config);
+        }
+
+        return $this;
     }
 
     /**
@@ -52,198 +65,159 @@ class LanguageManager extends AbstractManager
      *
      * Settings:
      * - languages
-     * - default_lang
+     * - default_language
+     * - current_language
      *
+     * @param  array $config The raw configuration array provided upon instantiation.
      * @return void
+     * @throws RuntimeException If the languages passed to the manager isn't an associative array.
+     * @todo   Implement cache get/set of JSON data based on languages.
      */
-    public function setup_translations()
+    public function setup_languages(array $config)
     {
-        $config = $this->config();
-
-        $translation_config = [];
+        $langs = [];
 
         if (isset($config['default_language'])) {
-            $translation_config['default_language'] = $config['default_language'];
+            $langs['default_language'] = $config['default_language'];
         }
 
-        if (isset($config['languages'])) {
-            $available_langs = array_filter($config['languages'], function ($config) {
-                return (!isset($config['active']) || $config['active']);
-            });
+        /** Not recommended; allow the current language to be determined by the client. */
+        if (isset($config['current_language'])) {
+            $langs['current_language'] = $config['current_language'];
+        }
 
-            if (count($available_langs)) {
-                $translation_config['languages'] = array_keys($available_langs);
+        /**
+         * Build the array of Language objects from the `TranslationConfig`-filtered list
+         * to prevent any bad apples from slipping through.
+         */
+        if (isset($config['languages'])) {
+            if (is_array($config['languages'])) {
+                $config['languages'] = array_filter($config['languages'], function ($config) {
+                    return (!isset($config['active']) || $config['active']);
+                });
+
+                if (count($config['languages'])) {
+                    $index = self::language_index(array_keys($config['languages']));
+
+                    $langs['languages'] = [];
+                    foreach ($config['languages'] as $ident => $data) {
+                        $lang = new Language();
+
+                        if (!is_array($data)) {
+                            $data = [];
+                        }
+
+                        if (isset($index[$ident])) {
+                            $data = array_merge($index[$ident], $data);
+                        }
+
+                        if (!isset($data['ident'])) {
+                            $lang->set_ident($ident);
+                        }
+
+                        $lang->set_data($data);
+
+                        $langs['languages'][$lang->ident()] = $lang;
+                    }
+                }
+            } else {
+                throw new RuntimeException('Languages must be an associative array (e.g., `$langCode => $langInfo`).');
             }
         }
 
-        $this->set_translation($translation_config);
+        $translator = new TranslationConfig($langs);
+        $this->set_config($translator);
     }
 
     /**
-     * Set up the available languages, defaults, and active
+     * Set up global string translations
      *
+     * Settings:
+     * - translations
+     *
+     * @param  array $config The raw configuration array provided upon instantiation.
      * @return void
+     * @throws RuntimeException If the translations passed to the manager isn't an associative array.
      */
-    public function setup_languages()
+    private function setup_translations(array $config)
+    {
+        $trans = [];
+
+        /**
+         * Build the array of Language objects from the `TranslationConfig`-filtered list
+         * to prevent any bad apples from slipping through.
+         */
+        if (isset($config['translations'])) {
+            if (is_array($config['translations'])) {
+                $trans = $config['translations'];
+            } else {
+                throw new RuntimeException(
+                    'Global translations must be an associative array (e.g., `$ident => $translations`).'
+                );
+            }
+        }
+
+        $catalog = new Catalog($trans);
+        $this->set_catalog($catalog);
+    }
+
+    /**
+     * Get the manager's translation catalog
+     *
+     * @param  CatalogInterface $catalog A catalog to hold translations for the manager.
+     * @return self
+     */
+    protected function set_catalog(CatalogInterface $catalog)
+    {
+        $this->catalog = $catalog;
+        return $this;
+    }
+
+    /**
+     * Get the manager's translation catalog
+     *
+     * @return CatalogInterface The manager's catalog of translations.
+     */
+    public function catalog()
+    {
+        return $this->catalog;
+    }
+
+    /**
+     * Alias of `ConfigurableInterface::config()`
+     *
+     * @return TranslationConfig The manager's translation configuration object.
+     * @throws RuntimeException If the manager hasn't been set up.
+     */
+    public function translation()
     {
         $config = $this->config();
-        $index  = self::language_index();
 
-        if (isset($config['languages'])) {
-            $available_langs = $config['languages'];
-        } else {
-            $available_langs = $this->translation()->available_langs();
+        if (!($config instanceof TranslationConfig)) {
+            throw new RuntimeException('Manager hasn’t been set up.');
         }
 
-        foreach ($available_langs as $a => $b) {
-            $ident = null;
-            $data  = [];
-            $lang  = new Language();
-
-            if (is_string($b)) {
-                $ident = $b;
-            } elseif (is_string($a)) {
-                $ident = $a;
-            }
-
-            if (is_array($b)) {
-                $data = $b;
-            }
-
-            if (isset($index[$ident])) {
-                $data = array_merge($index[$ident], $data);
-            }
-
-            if ($ident && !isset($data['ident'])) {
-                $lang->set_ident($ident);
-            }
-
-            $lang->set_data($data);
-
-            if ($lang->ident() === null) {
-                $this->resolve_ident($lang, $ident);
-            }
-
-            $this->add_language($lang);
-        }
+        return $config;
     }
 
     /**
      * Get a list of all existing languages, their names and translations,
      * codes in various standards, and directionality.
      *
+     * @param  string $cache_key If provided, returns a subset of language information.
+     *     Defaults to returning all language data.
      * @return GenericConfig
+     *
+     * @todo Implement cache get/set of JSON data based on languages.
      */
-    public static function language_index()
+    public static function language_index($cache_key = null)
     {
         if (!isset(self::$language_index)) {
-            self::$language_index = new GenericConfig(__DIR__.'/../../../../config/languages.json');
+            if ($cache_key || !$cache_key) {
+                self::$language_index = new GenericConfig(__DIR__.'/../../../../config/languages.json');
+            }
         }
 
         return self::$language_index;
-    }
-
-    /**
-     * Resolve a string identifier for a given Language.
-     *
-     * @param  LanguageInterface $lang  The instance that needs resolution.
-     * @param  mixed             $ident Optional key/index of $config in previous method.
-     * @throws InvalidArgumentException If the language ident is null.
-     * @return void
-     */
-    public function resolve_ident(LanguageInterface &$lang, $ident = null)
-    {
-        if (is_string($ident)) {
-            $lang->set_ident($ident);
-        } else {
-            if (count($codes)) {
-                $lang->set_ident($lang->code());
-            }
-        }
-
-        if ($lang->ident() === null) {
-            $name = (string)$lang;
-            if (!$name) {
-                $name = 'Language';
-            }
-            throw new InvalidArgumentException(
-                sprintf(
-                    '%s requires a string identifier (one of "ident", "%s").',
-                    $name,
-                    implode('", "', $tries)
-                )
-            );
-        }
-    }
-
-    /**
-     * Set the manager's available languages
-     *
-     * @param  LanguageInterface[] $languages The list of languages to add.
-     * @throws InvalidArgumentException If array has a member that isn't an instance of Language.
-     * @return self
-     */
-    public function set_languages(array $languages)
-    {
-        $this->languages = [];
-
-        foreach ($languages as $lang) {
-            if ($lang instanceof LanguageInterface) {
-                $this->add_language($lang);
-            } else {
-                throw new InvalidArgumentException(
-                    'Must be an instance of Language.'
-                );
-            }
-        }
-        return $this;
-    }
-
-    /**
-     * Add or update an available language to the manager.
-     *
-     * @param  LanguageInterface $lang The Language object to add.
-     * @return self
-     */
-    public function add_language(LanguageInterface $lang)
-    {
-        $this->languages[$lang->ident()] = $lang;
-        return $this;
-    }
-
-    /**
-     * Get the manager's list of available languages
-     *
-     * @return LanguageInterface[]
-     */
-    public function languages()
-    {
-        return $this->languages;
-    }
-
-    /**
-     * Set the manager's translations for TranslationConfig
-     *
-     * @param  array|TranslationConfig $translation The translation configuration.
-     * @return self
-     */
-    public function set_translation($translation)
-    {
-        if ($translation instanceof TranslationConfig) {
-            $this->translation_config = $translation;
-        } elseif (is_array($translation)) {
-            $this->translation_config = new TranslationConfig($translation);
-        }
-        return $this;
-    }
-
-    /**
-     * Get the manager's translations
-     *
-     * @return TranslationConfig
-     */
-    public function translation()
-    {
-        return $this->translation_config;
     }
 }
