7efc00b737687c1f20e2e068f1754cf1dd1412ef
Mathieu Ducharme <mat@locomotive.ca>
2015-12-09 19:59:46 -0500

    Amend: typo.

--- build/travis/phpdoc_ghpages.sh
+++ build/travis/phpdoc_ghpages.sh
@@ -19,7 +19,7 @@ if [ "$TRAVIS_REPO_SLUG" == "locomotivemtl/charcoal-app" ] && [ "$TRAVIS_PULL_RE
   ## Initialisation et recuperation de la branche gh-pages du depot Git
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "travvis-ci"
-  git clone --quiet --branch=gh-pages https://${GH_TOKEN}@$P{GH_REPO} gh-pages > /dev/null
+  git clone --quiet --branch=gh-pages https://${GH_TOKEN}@${GH_REPO} gh-pages > /dev/null
 
   cd gh-pages
 
