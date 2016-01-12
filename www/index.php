3830e2dddb4f5fdc9eb9378cef0058bc4ca62d7c
Mathieu Ducharme <mat@locomotive.ca>
2016-01-12 15:04:41 -0500

    Latest front-controller demo / example.

--- www/index.php
+++ www/index.php
@@ -4,10 +4,9 @@ ini_set('error_reporting', E_ALL);
 ini_set('display_errors', true);
 
 use \Charcoal\Charcoal;
-use \Charcoal\CharcoalConfig;
 use \Charcoal\App\App;
 use \Charcoal\App\AppConfig;
-use \Slim\Container as SlimContainer;
+use \Charcoal\App\AppContainer;
 
 // If using PHP's built-in server, return false for existing files on filesystem
 if (PHP_SAPI === 'cli-server') {
@@ -20,40 +19,16 @@ if (PHP_SAPI === 'cli-server') {
 // Require the Charcoal Framework
 include '../vendor/autoload.php';
 
+$settings = [];
+$config = new AppConfig();
+$config->add_file(__DIR__.'/../config/config.php');
+$config->set('ROOT', dirname(__DIR__) . '/');
+
 // Create container and configure it (with charcoal-config)
-$container = new SlimContainer();
-
-$container['charcoal/app/config'] = function($c) {
-    $config = new AppConfig();
-    $config->add_file(__DIR__.'/../config/config.php');
-    return $config;
-};
-
-// Handle "404 Not Found"
-$container['notFoundHandler'] = function ($c)
-{
-    return function ($request, $response) use ($c)
-    {
-        return $c['response']
-            ->withStatus(404)
-            ->withHeader('Content-Type', 'text/html')
-            ->write('Page not found'."\n");
-    };
-};
-
-// Handle "500 Server Error"
-$container['errorHandler'] = function ($c)
-{
-    return function ($request, $response, $exception) use ($c)
-    {
-        return $c['response']
-            ->withStatus(500)
-			->withHeader('Content-Type', 'text/html')
-			->write(
-                sprintf('Something went wrong! (%s)'."\n", $exception->getMessage())
-            );
-	};
-};
+$container = new AppContainer([
+    'settings'=>$settings,
+    'config'=>$config
+]);
 
 // Charcoal / Slim is the main app
 $app = new App($container);
@@ -62,13 +37,23 @@ $app = new App($container);
 require __DIR__.'/../config/dependencies.php';
 // Register middlewares
 require __DIR__.'/../config/middlewares.php';
-// Register routes
-require __DIR__.'/../config/routes.php';
 
-Charcoal::init([
+// Remove me. This is for backward compatibility.
+$container['config'] = function($c) {
+    $config = new \Charcoal\CharcoalConfig();
+    $config->add_file(__DIR__.'/../config/config.php');
+    $config->set('ROOT', dirname(__DIR__) . '/');
+    return $config;
+};
+$container['charcoal/config'] = $container['config'];
+\Charcoal\Charcoal::init([
     'app'    => $app,
-    'config' => new CharcoalConfig('../config/config.php'),
+    'config' => $container['charcoal/config'],
     'logger' => $container['logger']
 ]);
+
+if (!session_id()) {
+    session_start();
+}
 
 $app->run();
