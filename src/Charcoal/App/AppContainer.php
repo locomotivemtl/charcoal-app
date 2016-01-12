c7fdaf07b9bd5d80429725e9a125dd7768adfbe3
Mathieu Ducharme <mat@locomotive.ca>
2016-01-12 14:55:44 -0500

    Style fix.

--- src/Charcoal/App/AppContainer.php
+++ src/Charcoal/App/AppContainer.php
@@ -16,8 +16,8 @@ use \Stash\Driver\Ephemeral;
 use \Stash\Pool;
 
 /**
-* Charcoal App Container
-*/
+ * Charcoal App Container
+ */
 class AppContainer extends SlimContainer
 {
     /**
@@ -34,18 +34,19 @@ class AppContainer extends SlimContainer
     }
 
     /**
-    * @param AppConfig $config
-    */
+     * @param AppConfig $config The Charcoal App configuration.
+     * @return void
+     */
     private function register_defaults(AppConfig $config)
     {
         $this['charcoal/app/config'] = $config;
 
         // 404 Not found Handler
-        if(!isset($this['notFoundHandler]'])) {
-            $this['notFoundHandler'] = function ($c)
-            {
-                return function ($request, $response) use ($c)
-                {
+        if (!isset($this['notFoundHandler]'])) {
+            $this['notFoundHandler'] = function ($c) {
+
+                return function ($request, $response) use ($c) {
+
                     return $c['response']
                         ->withStatus(404)
                         ->withHeader('Content-Type', 'text/html')
@@ -55,11 +56,11 @@ class AppContainer extends SlimContainer
         }
 
         // 500 Error Handler
-        if(!isset($this['errorHandler'])) {
-            $this['errorHandler'] = function ($c)
-            {
-                return function ($request, $response, $exception) use ($c)
-                {
+        if (!isset($this['errorHandler'])) {
+            $this['errorHandler'] = function ($c) {
+
+                return function ($request, $response, $exception) use ($c) {
+
 
                     return $c['response']
                         ->withStatus(500)
@@ -72,20 +73,24 @@ class AppContainer extends SlimContainer
         }
 
         // PSR-3 logger, with Monolog.
-        if(!isset($this['logger'])) {
+        if (!isset($this['logger'])) {
             $this['logger'] = function ($c) {
                 $app_config = $c->get('charcoal/app/config');
                 $logger_config = $app_config->get('logger');
 
+
+                $uid_processor = new UidProcessor();
+                $handler = new StreamHandler('charcoal.app.log', Logger::DEBUG);
+
                 $logger = new Logger('Charcoal');
-                $logger->pushProcessor(new UidProcessor());
-                $logger->pushHandler(new StreamHandler('charcoal.app.log', Logger::DEBUG));
+                $logger->pushProcessor($uid_processor);
+                $logger->pushHandler($handler);
                 return $logger;
             };
         }
 
         // Stash cache driver
-        if(!isset($this['cache/driver'])) {
+        if (!isset($this['cache/driver'])) {
             $this['cache/driver'] = function ($c) {
                 $app_config = $c->get('charcoal/app/config');
                 $cache_config = $app_config->get('cache');
@@ -93,11 +98,11 @@ class AppContainer extends SlimContainer
                 $type = isset($cache_config['type']) ? $cache_config['type'] : 'memcache';
                 $prefix = isset($cache_config['prefix']) ? $cache_config['prefix'] : $app_config->get('project_name');
 
-                if($type == 'memcache') {
+                if ($type == 'memcache') {
 
-                    if(isset($cache_config['servers'])) {
+                    if (isset($cache_config['servers'])) {
                         $servers = [];
-                        foreach($cache_config['servers'] as $server) {
+                        foreach ($cache_config['servers'] as $server) {
                             $servers[] = array_values($server);
                         }
                     } else {
@@ -109,8 +114,7 @@ class AppContainer extends SlimContainer
                         'servers' => $servers,
                         'prefix_key' => $prefix
                     ]);
-                }
-                else {
+                } else {
                     $driver = new Ephemeral();
                 }
                 return $driver;
@@ -119,7 +123,7 @@ class AppContainer extends SlimContainer
 
         // PSR-6 caching, with Stash
         // (note: stash 0.13 is not yet 100% psr-6 compliant, but as of 2016-01-12 v1.0 is not released)
-        if(!isset($this['cache'])) {
+        if (!isset($this['cache'])) {
             $this['cache'] = function ($c) {
                 $driver = $c['cache/driver'];
                 $pool = new Pool($driver);
