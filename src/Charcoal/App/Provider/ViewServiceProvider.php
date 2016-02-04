7e7657ca7487eaa7b3003313893d158a66494d2d
Mathieu Ducharme <mat@locomotive.ca>
2016-02-04 17:38:53 -0500

    View service provider

--- src/Charcoal/App/Provider/ViewServiceProvider.php
+++ src/Charcoal/App/Provider/ViewServiceProvider.php
@@ -2,20 +2,43 @@
 
 namespace Charcoal\App\Provider;
 
+// Pimple dependencies
 use \Pimple\ServiceProviderInterface;
 use \Pimple\Container;
 
 // Module `charcoal-view` dependencies
+use \Charcoal\View\GenericView;
 use \Charcoal\View\Mustache\MustacheEngine;
+use \Charcoal\View\Mustache\MustacheLoader;
 use \Charcoal\View\Php\PhpEngine;
+use \Charcoal\View\Php\PhpLoader;
 use \Charcoal\View\PhpMustache\PhpMustacheEngine;
 use \Charcoal\View\Twig\TwigEngine;
-use \Charcoal\View\GenericView;
+use \Charcoal\View\Twig\TwigLoader;
+use \Charcoal\View\Renderer;
 use \Charcoal\View\ViewConfig;
 use \Charcoal\View\ViewInterface;
 
 /**
  * View Service Provider
+ *
+ * ## External dependencies
+ * - `config`
+ *   - The global / base config (`ConfigInterface`).
+ * - `logger`
+ *   - A PSR-3 loger.
+ *
+ * ## Services
+ * - `view/config`
+ *   - The global view config (`ViewConfig`).
+ * - `view`
+ *   - The default `ViewInterface` object, determined by `view/config`.
+ * - `view/renderer`
+ *   - A PSR-7 renderer using the default `view` object.
+ *
+ * ## Helpers
+ * - `view/engine`
+ *   - The default `EngineInterface` object, determined by `view/config`.
  */
 class ViewServiceProvider implements ServiceProviderInterface
 {
@@ -31,18 +54,74 @@ class ViewServiceProvider implements ServiceProviderInterface
     public function register(Container $container)
     {
         /**
-         * @param Container $containe A container instance.
+         * @param Container $container A container instance.
          * @return ViewConfig
          */
         $container['view/config'] = function (Container $container) {
             $config = $container['config'];
 
-            $viewConfig =  new ViewConfig($config->get('view'));
+            $viewConfig =  new ViewConfig($config['view']);
             return $viewConfig;
         };
 
+
+        $this->registerLoaderServices($container);
+        $this->registerEngineServices($container);
+        $this->registerViewServices($container);
+
+    }
+
+    /**
+    * @param Container $container The DI container.
+    * @return void
+    */
+    protected function registerLoaderServices(Container $container)
+    {
         /**
-         * @param Container $containe A container instance.
+         * @param Container $container A container instance.
+         * @return array The view loader dependencies array.
+         */
+        $container['view/loader/args'] = function (Container $container) {
+            return [
+                'logger'    => $container['logger'],
+                'base_path' => $container['config']['ROOT'],
+                'paths'     => $container['view/config']['path']
+            ];
+        };
+
+        /**
+         * @param Container $container A container instance.
+         * @return MustacheLoader
+         */
+        $container['view/loader/mustache'] = function (Container $container) {
+            return new MustacheLoader($container['view/loader/args']);
+        };
+
+        /**
+         * @param Container $container A container instance.
+         * @return PhpLoader
+         */
+        $container['view/loader/php'] = function (Container $container) {
+            return new PhpLoader($container['view/loader/args']);
+        };
+
+        /**
+         * @param Container $container A container instance.
+         * @return TwigLoader
+         */
+        $container['view/loader/twig'] = function (Container $container) {
+            return new TwigLoader($container['view/loader/args']);
+        };
+    }
+
+    /**
+    * @param Container $container The DI container.
+    * @return void
+    */
+    protected function registerEngineServices(Container $container)
+    {
+        /**
+         * @param Container $container A container instance.
          * @return array The engine dependencies array.
          */
         $container['view/engine/args'] = function (Container $container) {
@@ -54,41 +133,46 @@ class ViewServiceProvider implements ServiceProviderInterface
         };
 
         /**
-         * @param Container $containe A container instance.
+         * @param Container $container A container instance.
          * @return MustacheEngine
          */
         $container['view/engine/mustache'] = function (Container $container) {
-            return new MustacheEngine($container['view/engine/args']);
+            $engineOptions = $container['view/engine/args'];
+            $engineOptions['loader'] = $container['view/loader/mustache'];
+            return new MustacheEngine($engineOptions);
         };
 
         /**
-         * @param Container $containe A container instance.
+         * @param Container $container A container instance.
          * @return PhpEngine
          */
         $container['view/engine/php'] = function (Container $container) {
-            return new PhpEngine($container['view/engine/args']);
+            $engineOptions = $container['view/engine/args'];
+            return new PhpEngine($engineOptions);
         };
 
         /**
-         * @param Container $containe A container instance.
+         * @param Container $container A container instance.
          * @return PhpMustacheEngine
          */
         $container['view/engine/php-mustache'] = function (Container $container) {
-            return new PhpMustacheEngine($container['view/engine/args']);
+            $engineOptions = $container['view/engine/args'];
+            return new PhpMustacheEngine($engineOptions);
         };
 
         /**
-         * @param Container $containe A container instance.
+         * @param Container $container A container instance.
          * @return TwigEngine
          */
         $container['view/engine/twig'] = function (Container $container) {
-            return new TwigEngine($container['view/engine/args']);
+            $engineOptions = $container['view/engine/args'];
+            return new TwigEngine($engineOptions);
         };
 
         /**
          * The default view engine.
          *
-         * @param Container $containe A container instance.
+         * @param Container $container A container instance.
          * @return \Charcoal\View\EngineInterface
          */
         $container['view/engine'] = function (Container $container) {
@@ -96,28 +180,38 @@ class ViewServiceProvider implements ServiceProviderInterface
             $type = $viewConfig['default_engine'];
             return $container['view/engine/'.$type];
         };
+    }
 
+    /**
+    * @param Container $container The DI container.
+    * @return void
+    */
+    protected function registerViewServices(Container $container)
+    {
         /**
          * The default view instance.
          *
-         * @param Container $containe A container instance.
+         * @param Container $container A container instance.
          * @return ViewInterface
          */
         $container['view'] = function (Container $container) {
             $viewConfig = $container['view/config'];
             $engine = $container['view/engine'];
 
-            $view = new \Charcoal\View\GenericView([
+            $view = new GenericView([
                 'config'=>$viewConfig
             ]);
             $view->setEngine($engine);
             return $view;
         };
 
-        // PSR7-Renderer object
+        /**
+         * @param Container $container A container instance.
+         * @return Renderer
+         */
         $container['view/renderer'] = function (Container $container) {
             $view = $container['view'];
-            $renderer = new \Charcoal\View\Renderer([
+            $renderer = new Renderer([
                 'view'=>$view
             ]);
             return $renderer;
