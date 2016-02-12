dd908c815df9b66895db0e3b255c84a2bc485684
Chauncey McAskill <chauncey@locomotive.ca>
2016-02-12 18:29:12 -0500

    Error handlers are now conditional
    In case they've been set on the Container before this provider is registered.

--- src/Charcoal/App/Provider/AppServiceProvider.php
+++ src/Charcoal/App/Provider/AppServiceProvider.php
@@ -45,45 +45,49 @@ class AppServiceProvider implements ServiceProviderInterface
      */
     public function register(Container $container)
     {
-        /**
-         * HTTP 404 (Not found) handler.
-         *
-         * @param Container $container A container instance.
-         * @return callable
-         */
-        $container['notFoundHandler'] = function (Container $container) {
+        if (!isset($container['notFoundHandler'])) {
+            /**
+             * HTTP 404 (Not found) handler.
+             *
+             * @param Container $container A container instance.
+             * @return callable
+             */
+            $container['notFoundHandler'] = function (Container $container) {
 
-            return function (RequestInterface $request, ResponseInterface $response) use ($container) {
+                return function (RequestInterface $request, ResponseInterface $response) use ($container) {
 
-                return $container['response']
-                    ->withStatus(404)
-                    ->withHeader('Content-Type', 'text/html')
-                    ->write('Page not found'."\n");
+                    return $container['response']
+                        ->withStatus(404)
+                        ->withHeader('Content-Type', 'text/html')
+                        ->write('Page not found'."\n");
+                };
             };
-        };
+        }
 
-        /**
-         * HTTP 500 (Error) handler.
-         *
-         * @param Container $container A container instance.
-         * @return callable
-         */
-        $container['errorHandler'] = function (Container $container) {
+        if (!isset($container['notFoundHandler'])) {
+            /**
+             * HTTP 500 (Error) handler.
+             *
+             * @param Container $container A container instance.
+             * @return callable
+             */
+            $container['errorHandler'] = function (Container $container) {
 
-            return function (
-                RequestInterface $request,
-                ResponseInterface $response,
-                Exception $exception
-            ) use ($container) {
+                return function (
+                    RequestInterface $request,
+                    ResponseInterface $response,
+                    Exception $exception
+                ) use ($container) {
 
-                return $container['response']
-                    ->withStatus(500)
-                    ->withHeader('Content-Type', 'text/html')
-                    ->write(
-                        sprintf('Something went wrong! (%s)'."\n", $exception->getMessage())
-                    );
+                    return $container['response']
+                        ->withStatus(500)
+                        ->withHeader('Content-Type', 'text/html')
+                        ->write(
+                            sprintf('Something went wrong! (%s)'."\n", $exception->getMessage())
+                        );
+                };
             };
-        };
+        }
 
         /**
          * @param Container $container A container instance.
