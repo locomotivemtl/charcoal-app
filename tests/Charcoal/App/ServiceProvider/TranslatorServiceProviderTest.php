de119802165cc8ef31bcfc346d32a2003cb88e50
Chauncey McAskill <shimoroka@gmail.com>
2017-02-02 22:00:48 -0500

    Fixed TranslatorServiceProviderTest

--- tests/Charcoal/App/ServiceProvider/TranslatorServiceProviderTest.php
+++ tests/Charcoal/App/ServiceProvider/TranslatorServiceProviderTest.php
@@ -2,10 +2,20 @@
 
 namespace Charcoal\Tests\App\ServiceProvider;
 
+// From PHPUnit
 use \PHPUnit_Framework_TestCase;
 
+// From PSR-3
+use \Psr\Log\NullLogger;
+
+// From `tedivm/stash`
+use \Stash\Driver\BlackHole;
+use \Stash\Pool;
+
+// From Pimple
 use \Pimple\Container;
 
+// From 'charcoal-app'
 use \Charcoal\App\ServiceProvider\TranslatorServiceProvider;
 
 /**
@@ -17,10 +27,18 @@ class TranslatorServiceProviderTest extends PHPUnit_Framework_TestCase
     {
         $container = new Container([
             'config' => [
-                'base_path' => ''
-            ],
-            'cache' => null
+                'base_path' => realpath(__DIR__.'/../../..')
+            ]
         ]);
+        $container['cache'] = function ($container) {
+            $driver = new BlackHole();
+            $pool   = new Pool($driver);
+
+            return $pool;
+        };
+        $container['logger'] = function($c) {
+            return new NullLogger();
+        };
         $provider = new TranslatorServiceProvider();
         $provider->register($container);
 
