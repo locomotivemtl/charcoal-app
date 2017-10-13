09c1c821e5d58301a64eb241e0a2b08c274bf2bc
Chauncey McAskill <chauncey@locomotive.ca>
2017-10-13 08:58:12 -0400

    Update Config and Provider for Logger Service
    Changes:
    - Cleanup syntax, imports, DocBlocks
    
    LoggerConfig:
    - Improved documentation of properties and methods
    - Expanded setters for Handlers and Processors to pre-approve given values
    - Added default channel name as a constant, setter/getter
    - Improved unit tests
    
    LoggerServiceProvider:
    - Improved definition of "logger/config"

--- src/Charcoal/App/Config/LoggerConfig.php
+++ src/Charcoal/App/Config/LoggerConfig.php
@@ -2,50 +2,73 @@
 
 namespace Charcoal\App\Config;
 
-// Module `charcoal-config` dependencies
-use \Charcoal\Config\AbstractConfig;
+use InvalidArgumentException;
+
+// From 'charcoal-config'
+use Charcoal\Config\AbstractConfig;
 
 /**
- *
+ * Logger Configuration
  */
 class LoggerConfig extends AbstractConfig
 {
+    const DEFAULT_CHANNEL = 'charcoal';
+
     /**
-     * @var boolean $active
+     * Whether to enable or disable the logger service.
+     *
+     * @var boolean
      */
     private $active;
 
     /**
-     * @var array $handlers
+     * Record handler(s) to use.
+     *
+     * Whenever you add a record to the logger, it traverses the handler stack.
+     *
+     * @var array
      */
     private $handlers;
 
     /**
-     * @var array $processors
+     * Record processor(s) to use.
+     *
+     * For customizing records added to the logger.
+     *
+     * @var array
      */
     private $processors;
 
     /**
+     * Channel name.
+     *
+     * @var string
+     */
+    private $channel = self::DEFAULT_CHANNEL;
+
+    /**
+     * Retrieve the default values.
+     *
      * @return array
      */
     public function defaults()
     {
         return [
-            'active' => true,
-            'namespace' => 'Charcoal',
+            'active'    => true,
+            'channel'   => self::DEFAULT_CHANNEL,
             'level'     => 'debug',
-            'handlers' => [
+            'handlers'  => [
                 'stream' => [
-                    'type'      => 'stream',
-                    'stream'    => '../charcoal.app.log',
-                    'level'     => null,
-                    'bubble'    => true,
-                    'active'    => true
+                    'type'   => 'stream',
+                    'stream' => '../charcoal.app.log',
+                    'level'  => null,
+                    'bubble' => true,
+                    'active' => true
                 ],
                 'console' => [
-                    'type'      => 'browser-console',
-                    'level'     => null,
-                    'active'    => false
+                    'type'   => 'browser-console',
+                    'level'  => null,
+                    'active' => false
                 ]
             ],
             'processors' => [
@@ -60,7 +83,10 @@ class LoggerConfig extends AbstractConfig
     }
 
     /**
-     * @param boolean $active The active flag.
+     * Enable / Disable the logger service.
+     *
+     * @param  boolean $active The active flag;
+     *     TRUE to enable, FALSE to disable.
      * @return LoggerConfig Chainable
      */
     public function setActive($active)
@@ -70,7 +96,9 @@ class LoggerConfig extends AbstractConfig
     }
 
     /**
-     * @return boolean
+     * Determine if the logger service is enabled.
+     *
+     * @return boolean TRUE if enabled, FALSE if disabled.
      */
     public function active()
     {
@@ -78,16 +106,60 @@ class LoggerConfig extends AbstractConfig
     }
 
     /**
-     * @param array $handlers The (monolog) logger handlers.
+     * Set the record handler(s) to use.
+     *
+     * @param  array $handlers One or more (Monolog) record handlers; used as a stack.
      * @return LoggerConfig Chainable
      */
     public function setHandlers(array $handlers)
     {
-        $this->handlers = $handlers;
+        $this->handlers = [];
+        $this->addHandlers($handlers);
         return $this;
     }
 
     /**
+     * Add record handler(s) to use.
+     *
+     * @param  string[] $handlers One or more (Monolog) handlers to stack.
+     * @return LoggerConfig Chainable
+     */
+    public function addHandlers(array $handlers)
+    {
+        foreach ($handlers as $key => $handler) {
+            $this->addHandler($handler, $key);
+        }
+        return $this;
+    }
+
+    /**
+     * Add a record handler to use.
+     *
+     * @param  array       $handler The record handler structure.
+     * @param  string|null $key     The handler's key.
+     * @throws InvalidArgumentException If the handler is invalid.
+     * @return LoggerConfig Chainable
+     */
+    public function addHandler(array $handler, $key = null)
+    {
+        if (!isset($handler['type'])) {
+            throw new InvalidArgumentException(
+                'Handler type is required.'
+            );
+        }
+
+        if (!is_string($key)) {
+            $this->handlers[] = $handler;
+        } else {
+            $this->handlers[$key] = $handler;
+        }
+
+        return $this;
+    }
+
+    /**
+     * Retrieve the record handler(s) to use.
+     *
      * @return array
      */
     public function handlers()
@@ -96,20 +168,93 @@ class LoggerConfig extends AbstractConfig
     }
 
     /**
-     * @param array $processors The (monolog) logger processors.
+     * Set the record processor(s) to use.
+     *
+     * @param  array $processors One or more (Monolog) record processors; used as a stack.
      * @return LoggerConfig Chainable
      */
     public function setProcessors(array $processors)
     {
-        $this->processors = $processors;
+        $this->processors = [];
+        $this->addProcessors($processors);
         return $this;
     }
 
     /**
+     * Add record processor(s) to use.
+     *
+     * @param  string[] $processors One or more (Monolog) processors to stack.
+     * @return LoggerConfig Chainable
+     */
+    public function addProcessors(array $processors)
+    {
+        foreach ($processors as $key => $processor) {
+            $this->addProcessor($processor, $key);
+        }
+        return $this;
+    }
+
+    /**
+     * Add a record processor to use.
+     *
+     * @param  array       $processor The record processor structure.
+     * @param  string|null $key       The processor's key.
+     * @throws InvalidArgumentException If the processor is invalid.
+     * @return LoggerConfig Chainable
+     */
+    public function addProcessor(array $processor, $key = null)
+    {
+        if (!isset($processor['type'])) {
+            throw new InvalidArgumentException(
+                'Processor type is required.'
+            );
+        }
+
+        if (!is_string($key)) {
+            $this->processors[] = $processor;
+        } else {
+            $this->processors[$key] = $processor;
+        }
+
+        return $this;
+    }
+
+    /**
+     * Retrieve the record processor(s) to use.
+     *
      * @return array
      */
     public function processors()
     {
         return $this->processors;
+    }
+
+    /**
+     * Set the channel name.
+     *
+     * @param  string $name The channe name (namespace).
+     * @throws InvalidArgumentException If the channel name is not a string.
+     * @return LoggerConfig Chainable
+     */
+    public function setChannel($name)
+    {
+        if (!is_string($name)) {
+            throw new InvalidArgumentException(
+                'Channel name must be a string.'
+            );
+        }
+
+        $this->channel = $name;
+        return $this;
+    }
+
+    /**
+     * Retrieve the cache namespace.
+     *
+     * @return string
+     */
+    public function channel()
+    {
+        return $this->channel;
     }
 }
