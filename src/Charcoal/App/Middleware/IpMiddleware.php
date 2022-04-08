38a10b374a6f1654a711125ef2133c7de6e5c959
Mathieu Ducharme <ducharme.mathieu@gmail.com>
2022-04-07 21:14:09 -0400

    Improvement to the IP middleware:
    - Renamed parameters blacklist & whitelist for disallowed / allowed.
    - Added error_message parameters that is output in the body along with a 403.
    - Typehint fixes.

--- src/Charcoal/App/Middleware/IpMiddleware.php
+++ src/Charcoal/App/Middleware/IpMiddleware.php
@@ -12,29 +12,34 @@ use Psr\Http\Message\ResponseInterface;
 class IpMiddleware
 {
     /**
-     * @var array|null
+     * @var ?string[]
      */
-    private $blacklist;
+    private $disallowed;
 
     /**
-     * @var array|null
+     * @var ?string[]
      */
-    private $whitelist;
-
-    /**
-     * @var string
-     */
-    private $blacklistedRedirect = '';
+    private $allowed ;
 
     /**
      * @var string
      */
-    private $notWhitelistedRedirect = '';
+    private $errorMessage;
 
     /**
-     * @var boolean
+     * @var string
      */
-    private $failOnInvalidIp;
+    private $disallowedRedirect;
+
+    /**
+     * @var string
+     */
+    private $notAllowedRedirect;
+
+    /**
+     * @var bool
+     */
+    private $failOnInvalidIp ;
 
     /**
      * @param array $data Constructor dependencies and options.
@@ -43,11 +48,13 @@ class IpMiddleware
     {
         $data = array_merge($this->defaults(), $data);
 
-        $this->blacklist = $data['blacklist'];
-        $this->whitelist = $data['whitelist'];
+        $this->disallowed = $data['disallowed'];
+        $this->allowed = $data['allowed'];
 
-        $this->blacklistedRedirect    = $data['blacklisted_redirect'];
-        $this->notWhitelistedRedirect = $data['not_whitelisted_redirect'];
+        $this->errorMessage = $data['error_message'];
+
+        $this->disallowedRedirect    = $data['disallowed_redirect'];
+        $this->notAllowedRedirect = $data['not_allowed_redirect'];
 
         $this->failOnInvalidIp = $data['fail_on_invalid_ip'];
     }
@@ -57,14 +64,16 @@ class IpMiddleware
      *
      * @return array
      */
-    public function defaults()
+    public function defaults() : array
     {
         return [
-            'blacklist' => null,
-            'whitelist' => null,
+            'disallowed' => [],
+            'allowed' => [],
 
-            'blacklisted_redirect'     => '',
-            'not_whitelisted_redirect' => '',
+            'error_message' => '',
+
+            'disallowed_redirect'     => '',
+            'not_allowed_redirect' => '',
 
             'fail_on_invalid_ip' => false
         ];
@@ -87,86 +96,94 @@ class IpMiddleware
      */
     public function __invoke(RequestInterface $request, ResponseInterface $response, callable $next)
     {
-        $ip = $this->getClientIp($request);
+        $ip = $this->getClientIp();
         if (!$ip) {
-            if ((!empty($this->blacklist) || !empty($this->whitelist)) && $this->failOnInvalidIp === true) {
+            if ((!empty($this->disallowed) || !empty($this->allowed)) && $this->failOnInvalidIp === true) {
+                if ($this->errorMessage) {
+                    $response->getBody()->write($this->errorMessage);
+                }
                 return $response->withStatus(403);
             } else {
                 return $next($request, $response);
             }
         }
 
-        // Check blacklist.
-        if ($this->isIpBlacklisted($ip) === true) {
-            if ($this->blacklistedRedirect !== '') {
+        // Check disallowed.
+        if ($this->isIpDisallowed($ip) === true) {
+            if ($this->disallowedRedirect !== '') {
                 return $response
                     ->withStatus(302)
-                    ->withHeader('Location', $this->blacklistedRedirect);
+                    ->withHeader('Location', $this->disallowedRedirect);
             } else {
-                // IP explicitely blacklisted: forbidden
+                // IP explicitely disallowed: forbidden
+                if ($this->errorMessage) {
+                    $response->getBody()->write($this->errorMessage);
+                }
                 return $response->withStatus(403);
             }
         }
 
-        // Check whitelist.
-        if ($this->isIpWhitelisted($ip) === false) {
-            if ($this->notWhitelistedRedirect !== '') {
+        // Check allowed.
+        if ($this->isIpAllowed($ip) === false) {
+            if ($this->notAllowedRedirect !== '') {
                 return $response
                     ->withStatus(302)
-                    ->withHeader('Location', $this->notWhitelistedRedirect);
+                    ->withHeader('Location', $this->notAllowedRedirect);
             } else {
-                // IP not whistelisted: forbidden
+                // IP not allowed:forbidden
+                if ($this->errorMessage) {
+                    $response->getBody()->write($this->errorMessage);
+                }
                 return $response->withStatus(403);
             }
         }
 
-        // If here, not blacklisted or not-whitelisted; continue as normal.
+        // If here, not disallowed or not-allowed; continue as normal.
         return $next($request, $response);
     }
 
     /**
-     * Check wether a certain IP is explicitely blacklisted.
+     * Check whether a certain IP is explicitly disallowed.
      *
-     * If the blacklist is null or empty, then nothing is ever blacklisted (return false).
+     * If the disallowed is null or empty, then nothing is ever disallowed (return false).
      *
      * Note: this method only performs an exact string match on IP address, no IP masking / range features.
      *
-     * @param string $ip The IP address to check against the blacklist.
+     * @param string $ip The IP address to check against the disallowed.
      * @return boolean
      */
-    private function isIpBlacklisted($ip)
+    private function isIpDisallowed($ip) : bool
     {
-        if (empty($this->blacklist)) {
+        if (empty($this->disallowed)) {
             return false;
         }
-        return $this->isIpInRange($ip, $this->blacklist);
+        return $this->isIpInRange($ip, $this->disallowed);
     }
 
     /**
-     * Check wether a certain IP is explicitely whitelisted.
+     * Check whether a certain IP is explicitly allowed.
      *
-     * If the whitelist is null or empty, then all IPs are whitelisted (return true).
+     * If the allowed is null or empty, then all IPs are allowed (return true).
      *
      * Note; This method only performs an exact string match on IP address, no IP masking / range features.
      *
-     * @param string $ip The IP address to check against the whitelist.
+     * @param string $ip The IP address to check against the allowed.
      * @return boolean
      */
-    private function isIpWhitelisted($ip)
+    private function isIpAllowed($ip) : bool
     {
-        if (empty($this->whitelist)) {
+        if (empty($this->allowed)) {
             return true;
         }
-        return $this->isIpInRange($ip, $this->whitelist);
+        return $this->isIpInRange($ip, $this->allowed);
     }
-
-
+    
     /**
      * @param string   $ip    The IP to check.
      * @param string[] $cidrs The array of IPs/CIDRs to validate against. "/32" netmask is assumed.
-     * @return boolean
+     * @return bool
      */
-    private function isIpInRange($ip, array $cidrs)
+    private function isIpInRange(string $ip, array $cidrs) : bool
     {
         foreach ($cidrs as $range) {
             if (strpos($range, '/') === false) {
@@ -187,10 +204,8 @@ class IpMiddleware
      * @param RequestInterface $request The PSR-7 HTTP request.
      * @return string
      */
-    private function getClientIp(RequestInterface $request)
+    private function getClientIp() : string
     {
-        unset($request);
-
         if (isset($_SERVER['REMOTE_ADDR'])) {
             return filter_var($_SERVER['REMOTE_ADDR'], FILTER_VALIDATE_IP);
         }
